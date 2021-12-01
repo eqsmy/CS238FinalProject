8371dc3edee1b292f137fa5f1fefd93802840e10
eqsmy <elysiaqs@gmail.com>
2021-12-01 12:15:26 -0800

    small bug fix in evaluate

--- policyeval.py
+++ policyeval.py
@@ -23,6 +23,6 @@ class MonteCarloPolicyEvaluation:
         self.m = num_samples
 
     def evaluate(self, pi, theta):
-        start_state = new_start_state()
-        result = [rollout(start_state, pi, self.d, theta) for i in range(0, self.m)]
+        #start_state = new_start_state()
+        result = [rollout(new_start_state(), pi, self.d, theta) for i in range(0, self.m)]
         return np.mean(result)
